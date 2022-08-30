e045209206adb346db4915914e0e2798472169a2
Hussein Omar <husseinomar6190@gmail.com>
2022-08-30 16:05:52 +0300

    adding a home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import Card from '../components/Card';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  gap: 40px;
+  flex-wrap: wrap;
+`;
+
+const Home = () => {
+  return (
+    <Container>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </Container>
+  );
+};
+
+export default Home;
